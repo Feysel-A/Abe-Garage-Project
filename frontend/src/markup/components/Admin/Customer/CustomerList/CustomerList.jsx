6d30a823dfb8b14a995e134c1a75a13b613447ab
asmid <ansarfeysel@gmail.com>
2024-12-04 23:36:06 +0300

    Customer list page updated

--- frontend/src/markup/components/Admin/Customer/CustomerList/CustomerList.jsx
+++ frontend/src/markup/components/Admin/Customer/CustomerList/CustomerList.jsx
@@ -7,6 +7,8 @@ import { FaEdit } from "react-icons/fa";
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 function CustomerList() {
   const [customers, setCustomers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredCustomers, setFilteredCustomers] = useState([]);
   const navigator = useNavigate();
   useEffect(() => {
     customerService
@@ -18,7 +20,30 @@ function CustomerList() {
       })
       .catch((error) => console.error(error));
   }, [customers]);
-  console.log(customers);
+
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    if (query) {
+      const filtered = customers.filter(
+        (customer) =>
+          customer.customer_first_name
+            .toLowerCase()
+            .includes(query.toLowerCase()) ||
+          customer.customer_last_name
+            .toLowerCase()
+            .includes(query.toLowerCase()) ||
+          customer.customer_email.toLowerCase().includes(query.toLowerCase()) ||
+          customer.customer_phone_number
+            .toLowerCase()
+            .includes(query.toLowerCase())
+      );
+      setFilteredCustomers(filtered);
+    } else {
+      setFilteredCustomers([]);
+    }
+  };
+  // console.log(customers);
   return (
     <>
       <section className="contact-section">
@@ -26,58 +51,114 @@ function CustomerList() {
           <div className="contact-title">
             <h2>customers</h2>
           </div>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Added Date</th>
-                <th>Actives</th>
-                <th>Edit</th>
-              </tr>
-            </thead>
-            <tbody>
-              {customers?.map((customer) => (
-                <tr key={customer.customer_id}>
-                  <td>{customer.customer_id}</td>
-                  <td>{customer.customer_first_name}</td>
-                  <td>{customer.customer_last_name}</td>
-                  <td>{customer.customer_email}</td>
-                  <td>{customer.customer_phone_number}</td>
-                  <td>
-                    {format(
-                      new Date(customer.customer_added_date),
-                      "MM - dd - yyyy | kk:mm"
-                    )}
-                  </td>
-                  <td>{customer.active_customer_status ? "Yes" : "No"}</td>
-                  <td>
-                    <div className="edit-delete-icons">
-                      <button
-                        onClick={() =>
-                          navigator(`/admin/customer/${customer.customer_hash}`)
-                        }
-                      >
-                        <FaEdit />
-                      </button>{" "}
-                      <button
-                        onClick={() =>
-                          navigator(
-                            `/admin/customer-profile/${customer.customer_hash}`
-                          )
-                        }
-                      >
-                        <FaArrowUpRightFromSquare />
-                      </button>
-                    </div>
-                  </td>
+          <div className="contact-form">
+            <div className="form-group col-md-12">
+              <input
+                type="text"
+                placeholder="Search by Name, Email, or Phone"
+                value={searchQuery}
+                onChange={handleSearch}
+              />
+            </div>
+
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Added Date</th>
+                  <th>Actives</th>
+                  <th>Edit</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody>
+                {filteredCustomers.length > 0
+                  ? filteredCustomers.map((customer) => (
+                      <tr key={customer.customer_id}>
+                        <td>{customer.customer_id}</td>
+                        <td>{customer.customer_first_name}</td>
+                        <td>{customer.customer_last_name}</td>
+                        <td>{customer.customer_email}</td>
+                        <td>{customer.customer_phone_number}</td>
+                        <td>
+                          {format(
+                            new Date(customer.customer_added_date),
+                            "MM - dd - yyyy | kk:mm"
+                          )}
+                        </td>
+                        <td>
+                          {customer.active_customer_status ? "Yes" : "No"}
+                        </td>
+                        <td>
+                          <div className="edit-delete-icons">
+                            <button
+                              onClick={() =>
+                                navigator(
+                                  `/admin/customer/${customer.customer_hash}`
+                                )
+                              }
+                            >
+                              <FaEdit />
+                            </button>{" "}
+                            <button
+                              onClick={() =>
+                                navigator(
+                                  `/admin/customer-profile/${customer.customer_hash}`
+                                )
+                              }
+                            >
+                              <FaArrowUpRightFromSquare />
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))
+                  : customers.map((customer) => (
+                      <tr key={customer.customer_id}>
+                        <td>{customer.customer_id}</td>
+                        <td>{customer.customer_first_name}</td>
+                        <td>{customer.customer_last_name}</td>
+                        <td>{customer.customer_email}</td>
+                        <td>{customer.customer_phone_number}</td>
+                        <td>
+                          {format(
+                            new Date(customer.customer_added_date),
+                            "MM - dd - yyyy | kk:mm"
+                          )}
+                        </td>
+                        <td>
+                          {customer.active_customer_status ? "Yes" : "No"}
+                        </td>
+                        <td>
+                          <div className="edit-delete-icons">
+                            <button
+                              onClick={() =>
+                                navigator(
+                                  `/admin/customer/${customer.customer_hash}`
+                                )
+                              }
+                            >
+                              <FaEdit />
+                            </button>{" "}
+                            <button
+                              onClick={() =>
+                                navigator(
+                                  `/admin/customer-profile/${customer.customer_hash}`
+                                )
+                              }
+                            >
+                              <FaArrowUpRightFromSquare />
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+              </tbody>
+            </Table>
+          </div>
         </div>
       </section>
     </>
